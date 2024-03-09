ac14a5e113b8032f697338fd15cd7565542e3cb6
lawngle <132254702+lawngle@users.noreply.github.com>
2024-03-08 18:15:17 -0800

    Continue bash script assignment

--- Chem150/script-assignment/vibir.sh
+++ Chem150/script-assignment/vibir.sh
@@ -1,3 +1,37 @@
 #! /bin/bash
+# Purpose: Report nonzero vib. frequencies and their IR intensities
 
-echo "meow :3"
+# help
+help() {
+	echo "Usage: vibir [OPTION]..."
+	echo
+	echo " -h, --help	Display this help message"
+	echo " -v, --verbose	Enable debug output"
+	echo " -o, --output	Specify output file (doesn't work yet)"
+	echo
+}
+
+# Varibales
+
+# Parse options
+while :; do
+	case $1 in
+		-h | --help)
+			help
+			exit ;;
+		-v | --verbose)
+			set -xv ;;
+		--)
+			shift
+			break ;;
+		-?*)
+			echo 'Unknown option (ignoring): ' "$1" >&2;;
+		*)
+			break ;;
+	esac
+	shift
+done
+
+
+# Collect frequency and IR intensity lines
+awk '/frequency\ \ /{print; nr[NR+6]; next}; NR in nr' force.out
